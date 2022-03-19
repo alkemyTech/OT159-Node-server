0189cf872b0f064380a6f81fd00c09ea3c4b063a
andres085 <andresito.martinez1985@gmail.com>
2022-03-19 18:20:57 -0300

    Armada la migracion para activities

---
+++ migrations/20220319211431-create_activities_table.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('activities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('activities');
+  }
+};
