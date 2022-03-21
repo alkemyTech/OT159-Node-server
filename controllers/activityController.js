762a8cb8f33bcea1be114418d5714049b8d16c09
andres085 <andresito.martinez1985@gmail.com>
2022-03-21 17:44:09 -0300

    Se modifico el controlador para dejarlo solo con las tareas solicitadas y se eliminaron las rutas

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -1,23 +1,9 @@
 const activitiesService = require('../services/activityService');
 
 exports.get = async (req, res) => {
-    try {
-        const activities = await activitiesService.getAll();
-        res.status(200).send(activities);
-    } catch (error) {
-        res.status(500).send({
-            error: 'Could not get activities'
-        });
-    }
+   
 }
 
 exports.getById = async (req, res) => {
-    try {
-        const activity = await activitiesService.getById(req.params.id);
-        res.status(200).send(activity);
-    } catch (error) {
-        res.status(500).send({
-            error: 'Could not get activity'
-        });
-    }
+  
 }
--- routes/activities.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const activities = require('../controllers/activityController');
-
-router.get('/', activities.get);
-router.get('/:id', activities.getById);
-
-module.exports = router;
