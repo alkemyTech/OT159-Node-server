f6169307e9d7c7db8baa77500f19cf9f6bf09350
Luis Patricio Medina Vargas <luismedina@alu.frt.utn.edu.ar>
2022-03-22 20:56:33 -0300

    deleted test and swagger because it was not in the HU

--- routes/members.js
+++ routes/members.js
@@ -11,54 +11,7 @@ router.get('/', [],
 
 router.get('/:id', [],
 )
-/**
- * @swagger
- * components:
- *  schemas:
- *    Member:
- *       type: object
- *       properties:
- *          id:
- *           type: int  
- *          name:
- *            type: string
- *            description: the members' name
- *          facebookUrl:
- *            type: string
- *            description: the members' link to facebook
- *          instagramUrl:
- *            type: string
- *            description: the members' link to instagram
- *          image:
- *            type: string
- *            description: the members' Image
- *          description:
- *            type: string
- *            description: the members' description something
- *       required:
- *         - name
- *         - image
- *       example:
- *        name: Merecedez Estafania 
- *        image: "1"
- */
-/**
- * @swagger
- * /members/:
- *  post:
- *      summary: create a new member
- *      tags: [Member]
- *      requestBody:
- *         required: true 
- *         content:
- *          application/json:
- *           schema:
- *             type: object
- *             $ref: '#/components/schemas/Member' 
- *      responses:
- *        200:
- *           description: new user created 
- */
+
 router.post('/', [
     check('name', 'The name is a string').isString(),
     check('name', 'the name is required').notEmpty(),
