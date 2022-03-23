b9280c34a86a461926454d065e06394d4f8bb478
Luisina Elisa Escobar Gentile <luisinaescobar5@gmail.com>
2022-03-23 08:56:05 -0300

    added categoryid in controller

--- controllers/controllerNews.js
+++ controllers/controllerNews.js
@@ -6,6 +6,7 @@ const controllerNews = async function (req, res, next) {
         await check('name').notEmpty().run(req);
         await check('image').notEmpty().run(req);
         await check('content').notEmpty().run(req);
+        //await check('categoryId').notEmpty().run(req);
         const result = validationResult(req);
         if (!result.isEmpty()) {
             return res.status(400).json({ errors: result.array() });
