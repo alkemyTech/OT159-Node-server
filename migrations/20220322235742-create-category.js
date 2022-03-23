32eaaf75238f8e446e1a09c625bc5926405eae36
Omaro Aburto Leiva <70820516+omaroaburto@users.noreply.github.com>
2022-03-23 19:32:36 -0300

    update category model

--- migrations/20220322235742-create-category.js
+++ migrations/20220322235742-create-category.js
@@ -1,41 +1,46 @@
 'use strict';
-
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('Categories',{
-      id: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
-          allowNull: false,
-          autoIncrement: true,
+const {Model} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  
+  class Category extends Model {}
+  
+  Category.init({
+    id: {
+        type: DataTypes.INTEGER, 
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }, 
+    image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }, 
+    deletedAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+    createdAt: DataTypes.DATE
+  },{
+    hooks: {
+      beforeUpdate: (category, options, fn) => {
+        category.updatedAt = new Date();
+        fn(null, category);
       },
-      name: {
-          type: Sequelize.STRING,
-          unique: true,
-          allowNull: false,
-      },
-      description: {
-          type: Sequelize.STRING,
-          allowNull: true,
-      }, 
-      image: {
-          type: Sequelize.STRING,
-          allowNull: true,
-      }, 
-      createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE
-      },
-      updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE
-      },
-      deletedAt: {
-          type: Sequelize.DATE
-      },
-    });
+      beforeCreate: (category, options, fn) => {
+        category.createdAt = new Date();
+        fn(null,cagetory)
+      }
+    },
   },
-  async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('Categories');
-  }
+  { 
+    sequelize,
+    modelName: 'Category', 
+  });
+  return Category;
 };
