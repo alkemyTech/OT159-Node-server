2c253bc3605930d172a835fe7a59cc5cf37b0b2b
JuanAvalo <avalojuanma@gmail.com>
2022-03-25 17:38:21 -0300

    Added middleware to check is user is Admin

--- routes/testimonial.js
+++ routes/testimonial.js
@@ -2,7 +2,8 @@ var express = require("express");
 var router = express.Router();
 const testimonialController = require("../controllers/testimonial");
 const validateTestimonial = require("../middlewares/validations/testimonial");
+const {authorize} = require('../middlewares/checkRole')
 
-router.post("/", validateTestimonial, testimonialController.create);
+router.post("/", authorize(), validateTestimonial, testimonialController.create);
 
 module.exports = router;
