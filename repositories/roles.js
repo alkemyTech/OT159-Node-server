6934d1acd7c19342e4ca64aac3541061d619c898
lunov08 <lukitmate@gmail.com>
2022-03-25 02:45:20 -0300

    borra lo que esta fuera del ticket

--- repositories/roles.js
+++ repositories/roles.js
@@ -1,12 +1,2 @@
 const db = require('../models')
 
-module.exports.getAllRoles = async() => {
-    const roles = await db.Role.findAll()
-    return roles
-}
-
-module.exports.getRoleById = async(id) => {
-    const oneRole = await db.Role.findByPk(id)
-    return oneRole
-}
-
--- services/roles.js
+++ services/roles.js
@@ -1,10 +1,1 @@
 const rolesRepository = require('../repositories/roles')
-
-module.exports.getAllRoles = () => {
-    return rolesRepository.getAllRoles()
-}
-
-module.exports.getRoleById = (id) => {
-    return rolesRepository.getRoleById(id)
-}
-
