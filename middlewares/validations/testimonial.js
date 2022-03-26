626b9e9b424666215ad054e1bc1642b45b85792c
JuanAvalo <avalojuanma@gmail.com>
2022-03-25 23:43:00 -0300

    Removed content validation to match the model

--- middlewares/validations/testimonial.js
+++ middlewares/validations/testimonial.js
@@ -12,8 +12,6 @@ const errorWrapperFunction = (req, res, next) => {
 const validateTestimonial = [
   check("name").notEmpty().withMessage("Name can not be empty").bail(),
 
-  check("content").notEmpty().withMessage("Content can not be empty").bail(),
-
   errorWrapperFunction,
 ];
 
