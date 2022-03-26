429497cb5faa61f88858fc97b5412a081cbbc8b8
Omaro Aburto Leiva <70820516+omaroaburto@users.noreply.github.com>
2022-03-26 17:24:58 -0300

    update categoriesController
    se actualiza y se agrega desestructuración

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -2,7 +2,8 @@ const { categoryServiceCreate } = require('../services/categoriesService')
 
 exports.create = async (req, res) => {
     try {
-        const category = await categoryServiceCreate(req.body.name, req.body.description, req.body.image) 
+        const {name, description, image} =  req.body
+        const category = await categoryServiceCreate(name, description, image) 
         res.status(200).json({
             category,
             msg:"the category has been created"
