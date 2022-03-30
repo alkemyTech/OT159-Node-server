b2d4bb1a69c2a0f69c5b2d0eb76f4f388e6e9573
Luis Patricio Medina Vargas <luismedina@alu.frt.utn.edu.ar>
2022-03-29 22:31:40 -0300

    fixe files

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
 
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/members', membersRouter)
 // catch 404 and forward to error handler
@@ -35,7 +35,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+
   // render the error page
   res.status(err.status || 500);
   res.render('error');
