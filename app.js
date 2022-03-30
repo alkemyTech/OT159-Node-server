eb51326c7ccfb7191452481ba280959b163514ae
Luis Patricio Medina Vargas <luismedina@alu.frt.utn.edu.ar>
2022-03-29 22:29:05 -0300

    fixe files

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ require('dotenv').config()
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const membersRouter = require('./routes/members');
+
 const app = express();
 app.use(cors())
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,18 +22,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
 app.use('/members', membersRouter)
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
