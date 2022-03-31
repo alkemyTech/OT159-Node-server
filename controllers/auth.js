e564c2c71860ee609b0b03f2c5630b179f6fb206
lunov08 <lukitmate@gmail.com>
2022-03-30 22:34:10 -0300

    Cambia el campo image por photo en el modelo User

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,9 +3,9 @@ const passwordEncryption = require('../helpers/encryptionHelper')
 
 const register = async(req, res, next) => {
     try {
-        const { firstName, lastName, email, image, roleId, password } = req.body
+        const { firstName, lastName, email, photo, roleId, password } = req.body
         const passwordEncrypted = await passwordEncryption(password)
-        const newUser = await usersService.createNewUser({firstName, lastName, email, image, roleId, password: passwordEncrypted})
+        const newUser = await usersService.createNewUser({firstName, lastName, email, photo, roleId, password: passwordEncrypted})
         res.status(201).json({newUser})
     } catch (error) {
         next(error)
