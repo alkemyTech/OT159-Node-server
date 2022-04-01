63b21be39f68ae3f0897a268992876fef7262310
JuanAvalo <avalojuanma@gmail.com>
2022-04-01 12:22:49 -0300

    Fixed mispelled ControLer folder

--- routes/members.js
+++ routes/members.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const { postMembers}=require('../controlers/membersController');
+const { postMembers}=require('../controllers/membersController');
 const  {validateMember} = require('../middlewares/memberMidldlewar');
 const router = Router();
 
