dcf465236965f7419bceec86ff4e473abacd1578
FernandaElola <elolafernanda@gmail.com>
2022-04-01 13:54:34 -0300

    add middleware checkRole

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const { create, detail } = require('../controllers/categoriesController'); 
 const { validateCreate } = require('../validators/categories');
+const {authorize} = require('../middlewares/checkRole');
+
 
 //crear usuario
 router.post('/',validateCreate, create);
-router.get('/:id', detail);
+router.get('/:id', authorize(), detail);
 
 module.exports = router;
