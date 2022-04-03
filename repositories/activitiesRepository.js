43cfbd1c4d06e7e296746d9c82a40fa3611a55ab
lunov08 <lukitmate@gmail.com>
2022-04-03 07:56:05 -0300

    config update activities repository

--- repositories/activitiesRepository.js
+++ repositories/activitiesRepository.js
@@ -5,6 +5,22 @@ const createNewActivity = async (activityDataFields) => {
     return newActivity
 }
 
+const getActivityById = async (id) => {
+    const activity = await db.Activities.findByPk(id)
+    return activity
+} 
+
+const updateActivity = async (activityDataFields, id) => {
+    const activity = await db.Activities.update(activityDataFields, {
+        where: {
+            id
+        }
+    })
+    return activity
+}
+
 module.exports = { 
-    createNewActivity 
+    createNewActivity,
+    getActivityById,
+    updateActivity  
 }
