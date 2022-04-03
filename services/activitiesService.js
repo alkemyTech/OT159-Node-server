4696e577f22210351c5ff369e7f2aebe5d9d7c8c
lunov08 <lukitmate@gmail.com>
2022-04-03 07:56:26 -0300

    config update activities service

--- services/activitiesService.js
+++ services/activitiesService.js
@@ -4,6 +4,16 @@ const createNewActivity = (activityDataFields) => {
     return activitiesRepository.createNewActivity(activityDataFields)
 }
 
+const getActivityById = (id) => {
+    return activitiesRepository.getActivityById(id)
+}
+
+const updateActivity = (activityDataFields, id) => {
+    return activitiesRepository.updateActivity(activityDataFields, id)
+}
+
 module.exports = {
-    createNewActivity
+    createNewActivity,
+    getActivityById,
+    updateActivity
 }
