b2f2f884eb5b9300d808da2e2ca2dc8a4f9dbf8d
lunov08 <lukitmate@gmail.com>
2022-04-03 07:57:21 -0300

    add put method in activities route

--- routes/activities.js
+++ routes/activities.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { createNewActivity } = require('../controllers/activitiesController')
+const { createNewActivity, updateActivity } = require('../controllers/activitiesController')
 const { validateActivities } = require('../middlewares/validations/activities')
 
 router.post('/', validateActivities, createNewActivity)
+router.put('/:id', validateActivities, updateActivity)
 
 module.exports = router
