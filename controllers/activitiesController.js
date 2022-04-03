16e422a7e493a68cfde43ea17265471a8a959b18
lunov08 <lukitmate@gmail.com>
2022-04-03 04:25:45 -0300

    config activities controller

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -1,9 +1,15 @@
-const activitiesRepository = require('../repositories/activitiesRepository');
+const activitiesService = require('../services/activitiesService')
 
-exports.get = async (req, res) => {
-   
+const createNewActivity = async(req, res, next) => {
+    try {
+        const { name, content, image } = req.body
+        const newActivity = await activitiesService.createNewActivity({name, content, image})
+        res.status(201).json({newActivity})
+    } catch (error) {
+        next(error)
+    } 
 }
 
-exports.getById = async (req, res) => {
- 
+module.exports = {
+    createNewActivity
 }
