437bef86a9f3f6fafd7bdab544a003762a2d721f
JuanAvalo <avalojuanma@gmail.com>
2022-04-05 11:32:31 -0300

    Added option to send token through middleware

--- middlewares/tokenHandler.js
+++ middlewares/tokenHandler.js
@@ -6,25 +6,39 @@ const jwtExpirySeconds = 600;
 
 //Used to create and send a token to the user after a successfull LogIn
 const createToken = (username, role) => {
-    const token = jwt.sign({username, role}, jwtKey, {
-        algorithm: "HS256",
-        expiresIn: jwtExpirySeconds,
-    })
-    return token;
-}
+  const token = jwt.sign({ username, role }, jwtKey, {
+    algorithm: 'HS256',
+    expiresIn: jwtExpirySeconds,
+  });
+  return token;
+};
 
 //Verify if the auth token sent by the user is valid
 const verifyToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    if(!authHeader) res.status(401).send('Unauthorized');
-    jwt.verify(authHeader, jwtKey, function(error, decoded){
-        console.log(decoded)
-        if(error) return res.status(500).send({auth: false, message: error});
-        next()
-    })
-}
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) res.status(401).send('Unauthorized');
+  jwt.verify(authHeader, jwtKey, function (error, decoded) {
+    console.log(decoded);
+    if (error) return res.status(500).send({ auth: false, message: error });
+    next();
+  });
+};
+
+const sendToken = (req, res) => {
+  const user = req.user;
+  if (user) {
+    try {
+      const token = createToken(user);
+      return res.status(200).send({ user: user, token: token });
+    } catch (err) {
+      return res.status(500).send('Something went wrong.');
+    }
+  }
+  return res.status(500).send('Could not authenticate');
+};
 
 module.exports = {
-    createToken,
-    verifyToken
-}
+  createToken,
+  verifyToken,
+  sendToken,
+};
