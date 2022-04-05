cf5267a66722abc993b8cb6527fbbd10b2180b6e
FernandaElola <elolafernanda@gmail.com>
2022-04-05 15:05:38 -0300

    organize validation folders

--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { controllerAddNews } = require('../controllers/controllerNews');
-const ValidateInputNews = require('../middlewares/validations');
+const ValidateInputNews = require('../middlewares/validations/news');
 /* POST news. */
 router.post('/', ValidateInputNews,controllerAddNews);
 
