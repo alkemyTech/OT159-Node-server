9ed9779536aaa65be2351e57c1b7a80a38632035
Luis Patricio Medina Vargas <luismedina@alu.frt.utn.edu.ar>
2022-04-07 22:22:20 -0300

    added send email at the moment register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const usersService = require('../services/users')
 const passwordEncryption = require('../helpers/encryptionHelper')
+const sendWelcomeEmail = require('../services/welcomeEmail')
 
 const register = async(req, res, next) => {
     try {
@@ -7,6 +8,7 @@ const register = async(req, res, next) => {
         const passwordEncrypted = await passwordEncryption(password)
         const newUser = await usersService.createNewUser({firstName, lastName, email, photo, roleId, password: passwordEncrypted})
         res.status(201).json({newUser})
+        sendWelcomeEmail(process.env.API_KEY,email,from='estebanferreccio@gmail.com',process.env.TEMPLATE_ID)
     } catch (error) {
         next(error)
     }
