c7cd15169ff51df697bf54fe7f3fdbe4afc6554c
FernandaElola <elolafernanda@gmail.com>
2022-04-12 11:12:27 -0300

    delete line added by mistake

--- middlewares/validations/activities.js
+++ middlewares/validations/activities.js
@@ -1,5 +1,4 @@
 const { check, validationResult } = require('express-validator')
-const db = require('../')
 
 const errorWrapperFunction = (req, res, next) => {
     const result = validationResult(req);
