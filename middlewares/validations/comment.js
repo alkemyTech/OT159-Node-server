86469c5de1a30a5e32108c4477eaa3339131c8e1
Luisina Elisa Escobar Gentile <luisinaescobar5@gmail.com>
2022-04-12 08:41:39 -0300

    validation add comment

---
+++ middlewares/validations/comment.js
@@ -0,0 +1,15 @@
+const { check, validationResult } = require('express-validator')
+const errorWrapperFunction = require('.');
+
+const validateAddComment = [
+    check('userId', 'the id most be int').isNumeric(),
+    check('userId', 'can not be empty the id').notEmpty(),
+    check('newsId', 'the id most be int').isNumeric(),
+    check('newsId', 'can not be empty the id').notEmpty(),
+    check('body')
+        .notEmpty().withMessage('this field cannot be empty!').bail(),
+
+    errorWrapperFunction
+]
+
+module.exports = validateAddComment
--- routes/comment.js
+++ routes/comment.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { addComment } = require('../controllers/comment')
-router.post('/', addComment);
+const validateAddComment = require('../middlewares/validations/comment')
+router.post('/', validateAddComment, addComment);
 
 module.exports = router;
