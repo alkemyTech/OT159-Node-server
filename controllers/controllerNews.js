00937ce64facf9ff92bf1bd1b29222b90a577bc1
luispatriciomedinavargas <53265748+luispatriciomedinavargas@users.noreply.github.com>
2022-04-18 21:38:00 -0300

    Update controllerNews.js

--- controllers/controllerNews.js
+++ controllers/controllerNews.js
@@ -1,89 +1,85 @@
-const serviceNews = require('../services/news');
-const {request,response}=require('express');
-const { type, get } = require('express/lib/response');
+const { request, response } = require("express");
+const { memberServiceCreate, memberServiceList, memberServiceDelete } = require("../services/memberService");
 
-const controllerAddNews = async function (req, res, next) {
-  try {
-    const { name, content, image, categoryId } = req.body
-        const data = {
-            name,
-            content,
-            image,
-            categoryId
-        }
-        const newsCreated = await serviceNews.save(data);
-        res.status(201).json('The news were successfully saved ')
-        return newsCreated;
-  } catch (error) {
-    next(error);
-    throw new Error('something happened');
-  }
-};
+const postMembers = async (req = request, res = response) => {
 
-const getAllNews=async(req,res)=>{
-  const {page}=req.query;
-
-  const getAllNews = await serviceNews.getAllNews(page);
-  try {
-   return res.status(200).json({
-     getAllNews
-    })
-  } catch (error) {
-    return res.status(500).json({
-      msg:'Internal Server Error',
-  })
-  }
-}
-const newsPutController=async(req=request,res=response)=>{
-    const { id }=req.params
-    const { name, content, image /*, categoryId */ } = req.body
-    const data = {
+    const { 
         name,
-        content,
-        image
-        //, categoryId
-    }
-   
-    try {
+        facebookUrl,
+        instagramUrl,
+        linkedinUrl,
+        image,
+        description, 
+    } = req.body;
 
-     const putNew = await serviceNews.putNewService(id,data)
-
-        if(putNew[0]===0){
-            return res.status(404).json({
-                msg:`the ${id} does not exist or is wrong, please check it`
-            })}
-
-       return res.status(200).json({
-            msg:'the update was successfully'
-        })
-    
-    } catch (error) {
-     throw new Error('something was bad, please check it')
-    
-     }
+        const data={ 
+            name,
+            image,
+            facebookUrl,
+            instagramUrl,
+            linkedinUrl,
+            description,
+         }
+ 
+            try {
+                const newMember = await memberServiceCreate(data)
+                return res.status(201).json({
+                    newMember
+                })
+            } catch (error) {
+                
+                console.log(error)
+                return res.status(500).json({
+                    msg:'something happened, please call the admin,'
+                })
+            }
 }
 
-const remove = async (req, res) => {
-  try {
+const deleteMember = async (req, res) => {
     const { id } = req.params;
-    const removedNew = await serviceNews.remove(id);
-    return res.status(200).send(`Removed ${removedNew} rows with id ${id}`);
-  } catch (error) {
-    return res.status(error.code).send(error.name);
-  }
-};
-const controllerFindById = async function (req, res, next) {
     try {
-        const id = req.params.id
-        const findNew = await serviceNews.FindById(id);
-        if (findNew === null) {
-            res.status(404).json({ msg: 'not found' });
-        }
-        return res.status(200).json(findNew);
+        await memberServiceDelete(id);
+        return res.status(200).json({
+            msg: 'Member deleted successfully'
+        })
     } catch (error) {
-        next(error)
+        return res.status(error.status).send({
+            message: error.message
+        });
     }
 }
 
+const listMembers = {
 
-module.exports = { controllerAddNews, newsPutController, remove, controllerFindById,getAllNews };
+    list: async (req, res)  => {
+
+        try {
+
+            const allMembers = await memberServiceList();
+
+            let response = {
+                meta: {
+                    status : 200,
+                    total: allMembers.length,
+                    url: '/members'
+                },
+                data: allMembers  
+            }
+            
+            res.json(response);
+
+        } catch (error) {
+                
+            return res.status(500).json({
+                msg:'Internal Server Error'
+            })
+        }
+
+    }
+}
+
+module.exports = {
+    postMembers, 
+    listMembers,
+    deleteMember
+}
