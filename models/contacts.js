df4808a25041b1ac97ed653f9f2f2c1d28742719
Omaro Aburto Leiva <70820516+omaroaburto@users.noreply.github.com>
2022-04-19 12:58:40 -0400

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -36,8 +36,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "Contact",
-      paranoid: true,
-      deletedAt: "destroyContact",
+      paranoid: true, 
     }
   );
   return Contact;
