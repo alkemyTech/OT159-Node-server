30c5a228fd89a8feef12a137fb251acf1f61aa98
Luis Patricio Medina Vargas <luismedina@alu.frt.utn.edu.ar>
2022-04-19 19:42:59 -0300

    fixed something commented in PR

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,22 +5,21 @@ module.exports = {
   usersList: async (req, res) => {
    
   },
-  pathUser: async(req = request, res = response)=>{
+  editingUser: async(req = request, res = response)=>{
+
     const { firstName, lastName, email, password, image }=req.body;
     const { id }=req.params;
     const data={ firstName, lastName, email, password, image}
-  try {
-    await userService.patchUserById(id,data)
-    res.status(200).json({
-      msg:'The user was successfully edited'
-    })
+    try {
+      await userService.patchUserById(id,data)
+      res.status(200).json({
+        msg:'The user was successfully edited'
+      })} 
 
-  } 
-  catch (error) {
-  res.status(404).json({
-    msg:`New not Found, please check the ID value ${id}`
-  })
-  }
+    catch (error) {
+    res.status(404).json({
+      msg:`something was wrong, please check it. `
+    })}
 
   }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const { usersList,pathUser } = require('../controllers/usersController');
+const { usersList,editingUser } = require('../controllers/usersController');
 const validateUser = require('../middlewares/validations/user');
 
 /* GET users listing. */
 router.get('/users', usersList);
 
-router.patch('/:id', validateUser,pathUser);
+router.patch('/:id', validateUser,editingUser);
 
 module.exports = router;
