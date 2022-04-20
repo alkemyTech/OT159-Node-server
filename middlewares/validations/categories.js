8db5282b2017de290bc71721b2f215ae6b575e3e
FernandaElola <elolafernanda@gmail.com>
2022-04-20 20:56:04 -0300

    fix bug categories

--- middlewares/validations/categories.js
+++ middlewares/validations/categories.js
@@ -1,10 +1,9 @@
 const {check} = require('express-validator'); 
 const { validateCategory } = require('../validateCategories');
-const { isAdminRole, haveRole } = require('../validateRoles');
+const { isAdminRole } = require('../validateRoles');
 
 const validateCreate = [
     isAdminRole, // valida que el usuario tenga el rol de administrador
-    haveRole('ADMIN'), // valida tenga un rol de la lista
     check('name')
             .notEmpty()
             .withMessage('The name is required.') 
