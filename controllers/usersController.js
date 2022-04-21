61ab16ba20dc131db4eb2f45fbce0c2c019c8d60
Maxi Ceballos <maxi@MBP-de-Maxi.fibertel.com.ar>
2022-04-21 00:18:27 -0300

    move call service inside try/catch

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -26,12 +26,13 @@ const userService = require("../services/usersService");
       })
     }  
   }
+
 const editingUser = async(req = request, res = response)=>{
     const { firstName, lastName, email, password, image }=req.body;
     const { id }=req.params;
     const data={ firstName, lastName, email, password, image}
-    const checkPathUser = await userService.patchUserById(id,data)
     try {
+      const checkPathUser = await userService.patchUserById(id,data)
       if(checkPathUser[0]===0){
         return res.status(404).json({
           msg:`the user does not exist, please check the id : ${id}`
