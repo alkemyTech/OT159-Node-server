457556bced0aa99eb997bb7d1586f41210a9979e
luispatriciomedinavargas <53265748+luispatriciomedinavargas@users.noreply.github.com>
2022-04-20 21:14:42 -0300

    Update news.js

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const { controllerAddNews, controllerFindById, newsPutController, remove } = require('../controllers/controllerNews');
+const { controllerAddNews, newsPutController, remove } = require('../controllers/controllerNews');
 /* POST news. */
 const { haveRole } = require('../middlewares/validateRoles');
-const {ValidateInputNews,validateIdNews} = require('../middlewares/validations/news');
+const {ValidateInputNews,validateIdNews} = require('../middlewares/validations');
 
 const { authorize } = require('../middlewares/checkRole');
 
