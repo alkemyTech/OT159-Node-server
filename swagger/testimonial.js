5d3fd8bf2346a69bbc6686b57f4dd0784fb285ec
eferreccio <estebanferreccio@gmail.con>
2022-04-22 10:04:04 -0300

    adding put method into documentation

--- swagger/testimonial.js
+++ swagger/testimonial.js
@@ -57,3 +57,46 @@
  *         '500':
  *           description: Internal server error
  */
+
+/**
+ * @swagger
+ * /testimonials/{id}:
+ *   put:
+ *       summary: update tesmonials
+ *       tags: [Testimonial]
+ *       parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: string
+ *            required: true
+ *            description: the testimonial id
+ *       requestBody:
+ *          required: true
+ *          content:
+ *            application/json:
+ *              schema:
+ *                 type: object
+ *                 $ref: '#/components/schemas/Testimonial'
+ *              examples:
+ *                registro exitoso:
+ *                  value:    
+ *                    "name": "Aniversario Fundación Somos Más!"
+ *                    "image": "picture.jpg"
+ *                    "content" : "Este mes se hará un evento con motivo de festejar nuestro aniversario."
+ *                registro sin éxito (No contiene nombre):
+ *                  value:    
+ *                    "image": "picture.jpg"
+ *                    "content" : "Este mes se hará un evento con motivo de festejar nuestro aniversario."
+ *                registro sin éxito (No contiene texto):
+ *                  value:    
+ *                    "name": "Aniversario Fundación Somos Más!"
+ *                    "image": "picture.jpg"
+ *       responses:
+ *         '200':
+ *           description: the testimonial has been updated
+ *         '404':
+ *           description: the id 4 does not exist
+ *         '500':
+ *           description: the testimonial has not been updated
+ */
