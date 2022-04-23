1ab7b6056f48c75e8a5fa9eecd6cf2e7b831352f
andres085 <andresito.martinez1985@gmail.com>
2022-04-23 19:11:13 -0300

    Middleware de validar admin funcionando

--- middlewares/checkToken.js
+++ middlewares/checkToken.js
@@ -1,22 +1,23 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const db = require("../models");
 
-const checkToken = async (req, res, next) => {
-  let token = req.headers['authorization'];  
+const checkToken = (req, res, next) => {
+    let token = req.headers.bearer;
     if (!token){
-        return res.status(403).send({ msg: 'Token must be provided' });
+      return res.status(403).send({ msg: 'Token must be provided' });
     }
-    jwt.verify(token, process.env.SECRET, (err, decoded) => {
-        if (err){
-            return res.status(403).send({ msg: 'Invalid token' + err });
-        }
-        const user = await db.User.findOne({ where: { id:decoded.id } });                
-        if(!user){
-            res.status(403).json({status:"User not found"});
-        }
-        next();
-    });
+    const data = jwt.verify(token, process.env.SECRET);
+    return data.user;
+    // jwt.verify(token, process.env.SECRET, (err, decoded) => {
+    //     if (err){
+    //         return res.status(403).send({ msg: 'Invalid token' + err });
+    //     }
+    //     const user = await db.User.findOne({ where: { id: decoded.id } });                
+    //     if(!user){
+    //         res.status(403).json({status:"User not found"});
+    //     }
+    //     next();
+    // });
 }
 
 module.exports = checkToken;
--- middlewares/validateRoles.js
+++ middlewares/validateRoles.js
@@ -1,13 +1,16 @@
+const checkToken = require("./checkToken");
 
- const isAdminRole = (req, res, next) =>{
-    if(!req.user){
+const isAdminRole = (req, res, next) => {
+    const user = checkToken(req, res, next);
+    
+    if(!user){
         return res.status(500).json({
             msg: "You need to login first"
         });
     }
 
-    const roleId = req.user.roleId;
-    const name = req.user.name;
+    const roleId = user.roleId;
+    const name = user.firstName;
 
     if(roleId !== 1){
         return res.status(401).json({
