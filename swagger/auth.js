e085225585a913559f5c19b75621268d8c56b2b2
eferreccio <estebanferreccio@gmail.con>
2022-04-24 21:40:32 -0300

    user's tags changed

--- swagger/auth.js
+++ swagger/auth.js
@@ -29,7 +29,7 @@
  * @swagger
  * /auth/register:
  *   post:
- *       tags: ["Register User"]
+ *       tags: ["User"]
  *       summary: Sign Up User
  *       description: Register
  *       requestBody:
@@ -85,7 +85,7 @@
  * @swagger
  * /auth/login:
  *   post:
- *       tags: ["Login User"]
+ *       tags: ["User"]
  *       summary: Sign in User
  *       description: Login
  *       requestBody:
