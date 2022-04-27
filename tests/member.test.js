8adcb6516f4bc66571c78ab3af34c208440d780f
andres085 <andresito.martinez1985@gmail.com>
2022-04-27 10:30:55 -0300

    Test al endpoint delete de member con id funcionando

--- tests/member.test.js
+++ tests/member.test.js
@@ -18,13 +18,32 @@ beforeEach(async() => {
 describe('members endpoint', () => {
 
     test('can get all the members', async () => {
-      await api.get('/members')
+      const response = await api.get('/members')
         .set('Authorization', `Bearer ${token}`)
         .expect(200);
+
+      const activeMembers = response.body.data.length;
+      const member1 = response.body.data[0];
+      expect(activeMembers).toEqual(2);
+      expect(member1.name).toBe('Member1');
     })
 
     test('error 403 if Bearer token its not provided', async () => {
       const response = await api.get('/members').expect(403);
-      expect(response.text).toBe("{\"msg\":\"Token must be provided\"}")
+      const message = response.text;
+      expect(message).toBe("{\"msg\":\"Token must be provided\"}")
     })
+  
+  test('can delete a member by id', async () => {
+    const getMembers = await api.get('/members')
+        .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+    const activeMembers = getMembers.body.data.length;
+    expect(activeMembers).toEqual(2);
+    const id = getMembers.body.data[1].id;
+    const response = await api.delete(`/members/${id}`)
+        .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+    expect(response.body.msg).toBe('Member deleted successfully')
+  });
 });
